247577a5e0b9e6034fac55a249456a75b4f0f8c9
Valouz <v.djamei@hotmail.fr>
2024-10-04 12:43:16 +0200

    create fight class

--- Classes/Fight.js
+++ Classes/Fight.js
@@ -2,7 +2,6 @@ const Dragon = require("./Dragon");
 
 class Fight {
     constructor(hero, dragon) {
-        super(name, maxHealth, damage);
         if (!(hero instanceof Hero)){
             throw new Error('First parameter should be instance of Hero but is of type ' + typeof maxCooldown);
         }
@@ -15,7 +14,23 @@ class Fight {
     }
 
     start(){
-        console.log('blop')
+        while (this.dragon.isAlive() && this.hero.isAlive()){
+            this.dragon.takeDamage(this.hero.attack());
+            if (this.dragon.isAlive()){
+                if (this.dragon.currentCooldown == 0){
+                    this.hero.takeDamage(this.dragon.specialAttack());
+                } else {
+                    this.hero.takeDamage(this.dragon.attack());
+                }
+            }
+            this.turn =+ 1;
+            this.dragon.currentCooldown -= 1;
+        }
+        if (this.dragon.isAlive()){
+            return 'Dragon has won the battle, and now has this much gold : ' + this.dragon.lair;
+        } else {
+            return 'Dragon has died. Oh well. You should start again';
+        }
     }
 }
 
